bd2d5cdaed8f6450092567fd0d60903f204d81ed
Igor Veyner <igorveyner95@gmail.com>
2024-03-24 14:19:02 -0700

    feat: create basic layout with fixtures

--- app/components/FlowChart.tsx
+++ app/components/FlowChart.tsx
@@ -1,12 +1,40 @@
 'use client'
 
-import ReactFlow, { Background, Controls, Node } from 'reactflow';
+import ReactFlow, {
+  Background, Controls, Node,
+  ReactFlowProvider,
+  useEdgesState,
+  useNodesState
+} from 'reactflow';
 import 'reactflow/dist/style.css';
 import Draft12 from '../__fixtures__/2020-12.json';
 import { JsonSchemaParser } from '../JSONSchemaParser/JsonSchemaParser';
 
-export function FlowChart() {
-  const nodes = useNodes()
+import ELK from 'elkjs/lib/elk.bundled.js';
+import { useEffect } from 'react';
+import { useLayoutedElements } from './useLayoutedElements';
+
+export const elk = new ELK();
+
+export const FlowChart = () => {
+  return (
+    <ReactFlowProvider>
+      <FlowChartWithoutProviders />
+    </ReactFlowProvider>
+  );
+}
+
+const FlowChartWithoutProviders = () => {
+  // TODO: re-enable after creating edges from fixture
+  // const nodes = useNodes()
+  const [nodes, , onNodesChange] = useNodesState(initialNodesFixture);
+  const [edges, , onEdgesChange] = useEdgesState(initialEdgesFixture);
+  const { layoutElements } = useLayoutedElements();
+
+  useEffect(() => {
+    layoutElements()
+  }, [])
+
 
   return (
     <div style={{ height: "100vh", width: "100vw", position: 'relative', pointerEvents: 'all' }}>
@@ -16,6 +44,10 @@ export function FlowChart() {
             hideAttribution: true
           }}
           nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          fitView
         >
           <Background />
           <Controls />
@@ -25,7 +57,7 @@ export function FlowChart() {
   );
 }
 
-const useNodes = (): Node[] => {
+export const useNodes = (): Node[] => {
   const schema: JSON = Draft12 as unknown as JSON // Hardcoded for now
 
   const jsonSchemaParser = new JsonSchemaParser(schema)
@@ -33,3 +65,51 @@ const useNodes = (): Node[] => {
 
   return nodes;
 }
+
+const initialNodesFixture = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'input' },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: '2',
+    data: { label: 'node 2' },
+    position: { x: 0, y: 100 },
+  },
+  {
+    id: '2a',
+    data: { label: 'node 2a' },
+    position: { x: 0, y: 200 },
+  },
+  {
+    id: '2b',
+    data: { label: 'node 2b' },
+    position: { x: 0, y: 300 },
+  },
+  {
+    id: '2c',
+    data: { label: 'node 2c' },
+    position: { x: 0, y: 400 },
+  },
+  {
+    id: '2d',
+    data: { label: 'node 2d' },
+    position: { x: 0, y: 500 },
+  },
+  {
+    id: '3',
+    data: { label: 'node 3' },
+    position: { x: 200, y: 100 },
+  },
+];
+
+const initialEdgesFixture = [
+  { id: 'e12', source: '1', target: '2' },
+  { id: 'e13', source: '1', target: '3' },
+  { id: 'e22a', source: '2', target: '2a' },
+  { id: 'e22b', source: '2', target: '2b' },
+  { id: 'e22c', source: '2', target: '2c' },
+  { id: 'e2c2d', source: '2c', target: '2d' },
+];
---
+++ app/components/useLayoutedElements.tsx
@@ -0,0 +1,45 @@
+'use client';
+import { useReactFlow } from 'reactflow';
+import { elk } from './FlowChart';
+
+
+export const useLayoutedElements = () => {
+  const { getNodes, setNodes, getEdges, fitView } = useReactFlow();
+
+  const defaultOptions = {
+    'elk.algorithm': 'layered',
+    'elk.layered.spacing.nodeNodeBetweenLayers': 100,
+    'elk.spacing.nodeNode': 250,
+    'elk.direction': 'DOWN'
+  };
+
+  // TODO: improve typing
+  const layoutElements = () => {
+    const layoutOptions = { ...defaultOptions };
+    const graph = {
+      id: 'root',
+      layoutOptions: layoutOptions,
+      children: getNodes(),
+      edges: getEdges(),
+    };
+
+    // TODO: improve typing
+    elk.layout(graph as any).then(({ children }) => {
+
+      // TODO: improve typing
+      // By mutating the children in-place we saves ourselves from creating a
+      // needless copy of the nodes array.
+      children?.forEach((node: any) => {
+        node.position = { x: node.x, y: node.y };
+      });
+
+
+      setNodes(children || [] as any);
+      window.requestAnimationFrame(() => {
+        fitView();
+      });
+    });
+  };
+
+  return { layoutElements };
+};
